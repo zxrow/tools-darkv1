8b4d858fb4ac24d14eeb0a34b291d94103c63b94
Hacketindonedia1 <49530313+Hacketindonedia1@users.noreply.github.com>
2019-08-13 17:01:18 +0700

    New Upload

---
+++ tools-dark.sh
@@ -0,0 +1,168 @@
+clear
+echo
+echo
+python2 password.py
+echo
+clear
+echo
+cyan='\033[36;1m'
+ungu='\033[35;1m'
+figlet selamat | lolcat
+figlet datang | lolcat
+sleep 2
+clear
+echo $ungu "
+===================================="
+echo $cyan "
+          _/﹋\_
+          (ಠ益ಠ)
+          <,︻╦╤─ - - - MATI LOH AJG
+          _/﹋\_"
+echo $ungu "
+      █▀▀▀▀▀▀▀▀| ✔ |▀▀▀▀▀▀▀▀█"
+echo $cyan "
+       ✔Author  :Lao'neis agung
+       ✔Fb    :lao'neis agung
+       ✔Youtube :Hacker online"
+echo $ungu "
+      ┗─══─━══─| ✔ |─══━─══─┛"
+echo $ungu "
+===================================="
+sleep 2
+
+echo $cyan "
+┏━━━━━┫{menu dark fb}┣━━━━━┓
+┃
+┠──[0] untuk instal bahan agar work ✔
+┃
+┠──[1] Dark 1.6 ✔
+┃
+┠──[2] Dark 1.7 [-FREE-] ✔
+┃
+┠──[3] Dark 1.8 ✔
+┃
+┠──[4] Dark premium ✔
+┃
+┠──[5] Dark VIP ✔
+┃
+┠──[6] Dark pro ✔
+┃
+┠──[7] Dark Diamond ✔
+┃
+┠──[8] Dark v.1.2 ✔
+┠
+┠──[9] v2.Dark ✔
+┠
+┠──[10] Dark-Cyber11 ✔
+┠
+┗────[99] exit ✘"
+echo "\033[35;1m"
+read -p "root@Pilih Nomor > " bro
+if [ $bro = 0 ] || [ $bro]
+then
+clear
+pkg install ruby
+gem install lolcat
+pip2 install requests
+pip2 install mechanize
+pip2 install requirements
+sh yasin.sh
+fi
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+pkg install git
+git clone  https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+pkg install git
+git clone https://github.com/muhalvin/darkfb7
+cd darkfb7
+python2 dark.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+pkg install git
+git clone https://github.com/Mr-XsZ/Dark-Fb
+cd Dark-Fb
+python2 dark.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+pkg install git
+git clone https://github.com/TheMagizz/DarkPremium
+cd DarkPremium
+python2 DarkFB.py
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+pkg install git
+git clone https://github.com/m4rche3ll-cyber/dark-vpro
+cd dark-vpro
+python2 dark-vpro.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+git clone https://github.com/wira2611/Pro2611
+cd Pro2611
+python2 Pro.py
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+git clone https://github.com/wira2611/diamon
+ls
+cd diamon
+ls
+python2 diamond-1.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+git clone https://github.com/cyber2611/v1.2
+ls
+cd v1.2
+ls
+python2 Dragon-v1.2.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+git clone https://github.com/cyber2611/v2-dark
+cd v2.dark
+ls
+python2 v2.dark.py
+if
+
+if [ $bro = 10 ] [ $bro = 10 ]
+then
+clear
+git clone https://github.com/cyber2611/dark-cyber11
+cd dark-cyber11
+ls
+python2 kapten.py
+if
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+echo $cyan  "We Are Family"
+sleep 1
+exit
+fi
